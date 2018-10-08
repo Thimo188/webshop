119c0dcf73177c826d8019159a5a092c83e43d78
Thimo Franken <tfranken@tf-websites.nl>
2018-10-08 23:33:18 +0200

    Possibility to log out via navbarf

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -50,6 +50,7 @@
             <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
           </form>
+          @guest
           <li class="nav-item dropdown right">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           <img src="https://static.thenounproject.com/png/630729-200.png" height="40" alt=""/>
@@ -81,6 +82,23 @@
           <a class="dropdown-item" href="#">Forgot password?</a>
         </div>
       </li>
+      @else
+        <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
           <a href="{{ url('/cart')}}"><img src="https://www.seoclerk.com/pics/want28565-1jLOM31435502711.png" height="40" alt=""/></a>
           <a><img src="https://cdn3.iconfinder.com/data/icons/pyconic-icons-1-2/512/heart-outline-512.png" height="40" alt=""/></a>
         </ul>

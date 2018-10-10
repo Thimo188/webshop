b460baa13b70f4660e08417c4057bfd4ecd63e10
CharlieVS92 <31687886+CharlieVS92@users.noreply.github.com>
2018-10-10 21:31:32 +0200

    Update resources/views/layouts/app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -90,7 +90,7 @@
               {{ Auth::user()->name }} <span class="caret"></span>
             </a>
             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="{{url('/sidemenu')}}">Account</a>
+              <a class="dropdown-item" href="{{url('/account')}}">Account</a>
               <a class="dropdown-item" href="{{ route('logout') }}"
               onclick="event.preventDefault();
               document.getElementById('logout-form').submit();">

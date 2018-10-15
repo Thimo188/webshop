d63e94c8f860a3b2713f1b884b78274df29dc738
CharlieVS92 <31687886+CharlieVS92@users.noreply.github.com>
2018-10-15 12:11:29 +0200

    Finally Fixed Cards in AccountPage

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -8,7 +8,7 @@
 
 
     <div class="container mt-5">
-        <div class="jumbotron">
+        <div class="jumbotron" style="border:1px solid black">
             <div class="row">
                 <div class="col-md-4 col-xs-12 col-sm-6 col-lg-4">
                     <img src="https://www.svgimages.com/svg-image/s5/man-passportsize-silhouette-icon-256x256.png" alt="stack photo" class="img">
@@ -27,16 +27,19 @@
                               <li><p><i class="fa fa-envelope"></i> {{ Auth::user()->email }}</p></li>
                               <li><p><span class="glyphicon glyphicon-new-window one" style="width:50px;"></span><a href="#">www.example.com</p></a>
                             </ul>
+                          <div class="container">
+                            <button class="btn"><i class="fas fa-cog fa-2x"></i></button>
+                          </div>
+
                         </div>
                     </div>
                   </div>
                 </div>
 
 
-                <div class="container mt-5">
-                 <div class="row h-50 justify-content-center align-items-center">
-                       <div class="card-deck">
-                         <div class="col-xs-6 col-sm-4">
+                <div class="container-fluid mt-5">
+                 <div class="row" id="pep">
+                         <div class="col-sm-4 charlieshoogte">
                           <div class="card h-100 bg-white border-white">
                              <div class="card-header">Orders</div>
                              <div class="card-body">
@@ -45,15 +48,17 @@
                              </div>
                            </div>
                          </div>
-
-                           <div class="card bg-white border-white">
+                         <div class="col-sm-4 charlieshoogte">
+                           <div class="card h-100 bg-white border-white">
                              <div class="card-header">Account Preferences</div>
                              <div class="card-body">
                                <a href="#" class="btn">Change Password</a>
                                <a href="#" class="btn">Privacy</a>
                               </div>
                            </div>
-                          <div class="card bg-white border-white">
+                         </div>
+                         <div class="col-sm-4 charlieshoogte">
+                          <div class="card h-100 bg-white border-white">
                             <div class="card-header">Wishlist</div>
                              <div class="card-body">
                                <a href="#" class="btn"></a>
@@ -61,6 +66,7 @@
                            </div>
                            </div>
                          </div>
+                       </div>
                        </div>
                      </div>
 

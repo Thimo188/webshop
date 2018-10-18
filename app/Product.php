903f9b7e2d54539224d9088a148ff5ddfac1b745
Vincent van Tilborg <43572377+VincentvanTil@users.noreply.github.com>
2018-10-18 13:02:19 +0200

    Fixed ProductSizing function

--- app/Product.php
+++ app/Product.php
@@ -9,6 +9,10 @@ class Product extends Model
   protected $fillable=['product_name', 'product_description'];
   public function ProductTag()
   {
-  return $this->hasOne('App\Product_Tag');
+    return $this->hasOne('App\Product_Tag');
+  }
+  public function ProductSizing()
+  {
+    return $this->hasOne(Product_Size::class);
   }
 }

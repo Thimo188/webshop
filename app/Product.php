0bd08d1c973eccd220ce29ca7b4e7f5ae873c55b
CharlieVS92 <31687886+CharlieVS92@users.noreply.github.com>
2018-10-18 12:42:39 +0200

    Changed Uploadform

--- app/Product.php
+++ app/Product.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+  protected $fillable=['product_name', 'product_description'];
   public function ProductTag()
   {
   return $this->hasOne('App\Product_Tag');
--- resources/views/upload_form.blade.php
+++ resources/views/upload_form.blade.php
@@ -4,24 +4,43 @@
 @section('content')
 @include('notification')
 
-<form action="/upload" method="post" enctype="multipart/form-data">
-    {{ csrf_field() }}
-    Product name:
-    <br />
-    <input type="text" name="name" />
-    <br />
-    Product descirption:
-    <br />
-    <input type="text" name="description" />
-    <br /><br />
-    Product photos (can attach more than one):
-    <br />
-    <input type="file" name="photos[]" multiple />
-    <br /><br />
-    <input type="submit" value="Upload" />
+<div class="container-fluid">
+  <div class="row" id="up">
 
-</form>
+    <div class="col-sm-6 mt-5">
+    </div>
+
+<div class="col-sm-6 mt-5">
+
+    <div class="form-group">
+
+      <form action="/upload" method="post" enctype="multipart/form-data">
+      {{ csrf_field() }}
+      <label for="exampleInputText1">Product name</label>
+      <br />
+      <input type="text" class="form-control" name="name" />
+      <br />
+      <label for="exampleTextArea">Product description</label>
+      <br />
+      <textarea class="form-control" name="description" rows="3"></textarea>
+      <br />
+      <label for="exampleSelect2">Select Tags</label>
+      <select multiple class="form-control" id="exampleSelect2">
+        <option>Photography</option>
+        <option>3DArt</option>
+        <option>Illustrations</option>
+      </select>
+      <br />
+      Product photos (can attach more than one):
+      <br />
+      <input type="file" name="photos[]" multiple />
+      <br /><br />
+      <input type="submit" value="Upload" />
+
+    </form>
 
 <img src="{{asset('storage/product/kunst3.PNG')}}">
+</div>
+</div>
 
 @endsection

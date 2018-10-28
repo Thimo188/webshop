c615073e5a5009003d1f4ee271c842fa59e59a10
Omid <31657634+OmidSahebzadeh@users.noreply.github.com>
2018-10-28 22:43:25 +0100

    weird bug fix

--- resources/views/wishlist.blade.php
+++ resources/views/wishlist.blade.php
@@ -28,9 +28,9 @@
         <div class="card-body">
           <h5 class="card-title">{{ $wishlistitem->Product()->first()->product_name }}</h5>
           <p class="card-text">{{ $wishlistitem->Product()->first()->product_description }}</p>
-          <a href="{{ route('wishlist.show', [$wishlistitem->id]) }}" class="btn btn-lg btn-primary">Go to this product</a>
+          <a href="{{ url('/description', $wishlistitem->product_id) }}" class="btn btn-lg btn-primary">Go to this product</a>
           <a href="{{ route('wishlist.destroy', $wishlistitem->id)}}" class="btn btn-lg btn-danger"><i class="fas fa-trash-alt"></i></a>
-
+<!-- "{{ route('wishlist.show', [$wishlistitem->id]) }}" -->
         </div>
       </div>
     </div>

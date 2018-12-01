aca6dd3ad5e0c02dac2791de60839244c610931a
CharlieVS92 <31687886+CharlieVS92@users.noreply.github.com>
2018-12-01 09:31:12 +0100

    Upload Page
    When field is empty, remember input. Some minor fixes.

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -22,101 +22,109 @@ use App\Color;
 use App\Product_Category;
 use App\Product_Color;
 
+
 class UploadController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        $category_list = Category::all();
-        $colors = Color::all();
-        return view('upload_form', compact('category_list', $category_list, 'colors', $colors));
+  /**
+  * Display a listing of the resource.
+  *
+  * @return \Illuminate\Http\Response
+  */
+  public function index()
+  {
+    $category_list = Category::all();
+    $colors = Color::all();
+    return view('upload_form', compact('category_list', $category_list, 'colors', $colors));
+  }
+
+  /**
+  * Show the form for creating a new resource.
+  *
+  * @return \Illuminate\Http\Response
+  */
+  public function create()
+  {
+
+    return view('upload_form');
+  }
+
+  /**
+  * Store a newly created resource in storage.
+  *
+  * @param  \Illuminate\Http\Request  $request
+  * @return \Illuminate\Http\Response
+  */
+  public function store(Request $request)
+  {
+    $subscription = Subscription::where('user_id', Auth::user()->id);
+    $orders = Order::where('created_at', '>=', Carbon::now()->addMonths(-1))->count();
+    if($subscription->count() > 0) {
+      if($orders >= $subscription->get()->amount) {
+        return redirect()->back()->withErrors(['msg', 'You have reached your subscription limit for this month']);
+      }
     }
+    $validatedData = $this->validate($request, [
+      'name' => 'required|max:255',
+      'description' => 'required|max:300',
+      'photos' => 'required',
+      'photos.*' => 'image|mimes:jpeg,bmp,png,jpg',
+      'Tag1' => 'required',
+      'Tag2',
+      'Tag3',
+      'price' => 'required',
+      'category' => 'required',
+      'color' => 'required',
+      //'product_categories' => 'required'
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
+    ]);
+    //if $validatedData->fails = true then redirect with $errors
 
-        return view('upload_form');
+    $product = new Product();
+    $product->product_name=$validatedData['name'];
+    $product->product_description=$validatedData['description'];
+    $product->price=$validatedData['price'];
+    $product->user_id = Auth::user()->id;
+    $product->save();
+
+    //$user->roles()->attach($roleId);
+
+
+    $product_category = new Product_Category();
+    $product_category->category_id=$validatedData['category'];
+    $product_category->product_id=$product->id;
+    $product_category->save();
+    //dd($request->all());
+
+    $product_color = new Product_Color();
+    $product_color->color_id=$validatedData['color'];
+    $product_color->product_id=$product->id;
+    $product_color->save();
+
+    $product_tag = new Product_Tag();
+    $product_tag->name=$validatedData['Tag1'];
+    $product_tag->product_id=$product->id;
+    $product_tag->save();
+
+    $product_tag = new Product_Tag();
+    $product_tag->name=$validatedData['Tag2'];
+    $product_tag->product_id=$product->id;
+    $product_tag->save();
+
+    $product_tag = new Product_Tag();
+    $product_tag->name=$validatedData['Tag3'];
+    $product_tag->product_id=$product->id;
+    $product_tag->save();
+
+    foreach ($validatedData['photos'] as $key => $image) {
+      $product_image = new Product_Image;
+      $product_image->product_id=$product->id;
+      $filename = $image->getClientOriginalName();
+      $FilePath = 'public/product/';
+      $image->storeAs($FilePath, $filename);
+      $product_image->file= 'storage/product/' . $filename;
+      $product_image->save();
     }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-		$subscription = Subscription::where('user_id', Auth::user()->id);
-		$orders = Order::where('created_at', '>=', Carbon::now()->addMonths(-1))->count();
-		if($subscription->count() > 0) {
-			if($orders >= $subscription->get()->amount) {
-				return redirect()->back()->withErrors(['msg', 'You have reached your subscription limit for this month']);
-			}
-		}
-        $validatedData = $this->validate($request, [
-          'name' => 'required|max:255',
-          'description' => 'required|max:300',
-          'photos' => 'required',
-          'photos.*' => 'image|mimes:jpeg,bmp,png,jpg',
-          'Tag1' => 'required',
-          'Tag2' => 'required',
-          'price' => 'required',
-          'optcategory' => 'required',
-          'optcolor' => 'required',
-          //'product_categories' => 'required'
-
-        ]);
-
-        $product = new Product();
-        $product->product_name=$validatedData['name'];
-        $product->product_description=$validatedData['description'];
-        $product->price=$validatedData['price'];
-		    $product->user_id = Auth::user()->id;
-        $product->save();
-
-        //$user->roles()->attach($roleId);
-
-
-        $product_category = new Product_Category();
-        $product_category->category_id=$validatedData['optcategory'];
-        $product_category->product_id=$product->id;
-        $product_category->save();
-        //dd($request->all());
-
-        $product_color = new Product_Color();
-        $product_color->color_id=$validatedData['optcolor'];
-        $product_color->product_id=$product->id;
-        $product_color->save();
-
-        $product_tag = new Product_Tag();
-        $product_tag->name=$validatedData['Tag1'];
-        $product_tag->product_id=$product->id;
-        $product_tag->save();
-
-        $product_tag = new Product_Tag();
-        $product_tag->name=$validatedData['Tag2'];
-        $product_tag->product_id=$product->id;
-        $product_tag->save();
-
-        foreach ($validatedData['photos'] as $key => $image) {
-          $product_image = new Product_Image;
-          $product_image->product_id=$product->id;
-          $filename = $image->getClientOriginalName();
-          $FilePath = 'public/product/';
-          $image->storeAs($FilePath, $filename);
-          $product_image->file= 'storage/product/' . $filename;
-          $product_image->save();
-          }
-          return redirect(url('/upload'));
+    return redirect(url('/upload'));
 
 
 
@@ -125,56 +133,53 @@ class UploadController extends Controller
 
 
 
-        }
+  }
+
+
+  /**
+
+  * Display the specified resource.
+  *
+  * @param  int  $id
+  * @return \Illuminate\Http\Response
+  */
+  public function show($id)
+  {
 
 
 
+  }
+  /**
+  * Show the form for editing the specified resource.
+  *
+  * @param  int  $id
+  * @return \Illuminate\Http\Response
+  */
+  public function edit($id)
+  {
+    //
+  }
 
+  /**
+  * Update the specified resource in storage.
+  *
+  * @param  \Illuminate\Http\Request  $request
+  * @param  int  $id
+  * @return \Illuminate\Http\Response
+  */
+  public function update(Request $request, $id)
+  {
+    //
+  }
 
-    /**
-
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-
-
-
-    }
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+  /**
+  * Remove the specified resource from storage.
+  *
+  * @param  int  $id
+  * @return \Illuminate\Http\Response
+  */
+  public function destroy($id)
+  {
+    //
+  }
 }
--- resources/views/upload_form.blade.php
+++ resources/views/upload_form.blade.php
@@ -18,77 +18,90 @@
     <div class="form-group">
 
       <form action="{{ url('/upload') }}" method="post" enctype="multipart/form-data">
-      {{ csrf_field() }}
+        {{ csrf_field() }}
 
-    </div>
+      </div>
 
 
-  <div class="col-md-6 mt-5">
+      <div class="col-md-6 mt-5">
+
         Product photos (can attach more than one)
         <br />
         <br />
 
-      <div class="custom-file">
-        <input type="file" class="custom-file-input" name="photos[]" id="inputGroupFile02" multiple>
-        <label class="custom-file-label" for="inputGroupFile02">Choose file</label>
-      </div>
-      <br />
-      <br />
-
-
-      <label for="exampleInputText1">Product Title</label>
-      <br />
-      <input type="text" class="form-control" name="name" />
-      <br />
-      <br />
-      <label for="exampleTextArea">Description</label>
-      <br />
-      <textarea class="form-control" maxlength="300" name="description" rows="2"></textarea>
-      <br />
-      <label for="exampleInputText2">Tags</label>
-      <br />
-      <input type="text" class="form-control" name="Tag1" />
-      <br />
-      <input type="text" class="form-control" name="Tag2" />
-      <br />
-      <label for="exampleSelect2">Select Category</label>
-      @foreach($category_list as $category)
-      <div class="form-check">
-        <label class="form-check-label">
-          <input type="radio" class="form-check-input" name="optcategory" value="{{$category->id}}">{{$category->name}}
-        </label>
-      </div>
-
-      @endforeach
-
-    </div>
-  <div class="col-md-6 mt-5">
-    Select Color
-    <br />
-    <br />
-      @foreach($colors as $color)
-        <div class="form-check">
-          <label class="form-check-label">
-          <input type="radio" class="form-check-input" name="optcolor" value="{{$color->id}}">{{$color->name}}
-          </label>
+        <div class="custom-file">
+          <input type="file" name="photos[]" id="inputGroupFile02" multiple>
         </div>
 
-      @endforeach
-    <br />
-    <br />
-    <br />
-    <br />
-     <label for="exampleSelect4">Price</label>
-      <br />
-      <input type="number" value="0" min="0" step="0.01" data-number-to-fixed="2" data-number-stepfactor="100" class="form-control" name="price" id="c1" />
-      <br />
-      <br />
-      <button type="submit" class="btn btn-primary" value="Upload">Upload</button>
-      <br />
-      <br />
-    </form>
+        <br />
+        <br />
+
+
+        <label for="exampleInputText1">Product Name</label>
+        <br />
+        <input type="text" class="form-control" name="name" value="{{ old('name') }}" />
+        <br />
+        <br />
+        <label for="exampleTextArea">Description</label>
+        <br />
+        How would you describe your product?
+        <br />
+        <textarea class="form-control" maxlength="300" name="description" rows="2" value="{{ old('description')}}">{{ old('description')}}</textarea>
+        <br />
+        <label for="exampleInputText2">Tags</label>
+        <br />
+        How can others find your product?
+        <br />
+        <input type="text" class="form-control" name="Tag1" value="{{ old('Tag1')}}" />
+        <br />
+        <input type="text" class="form-control" name="Tag2" value="{{ old('Tag2')}}" />
+        <br />
+        <input type="text" class="form-control" name="Tag3" value="{{ old('Tag3')}}" />
+        <br />
+      </div>
+
+
+
+      <div class="col-md-6 mt-5">
+
+        <label for="exampleSelect2">Select Category</label>
+        <br />
+        Choose the category that is most suitable to your product
+        @foreach($category_list as $category)
+        <div class="form-check">
+          <label class="form-check-label">
+            <input type="radio" class="form-check-input" name="category" value="{{$category->id}}">{{$category->name}}
+          </label>
+        </div>
+        @endforeach
+
+        <br />
+
+        Select Color
+        <br />
+        Which colors do you see in your product?
+        <br />
+        <br />
+        <select class="form-control" name="color">
+          @foreach($colors as $color)
+          <option value="{{$color->color_id}}">{{$color->name}}</option>
+          @endforeach
+        </select>
+        <br />
+        <br />
+        <br />
+        <br />
+        <label for="exampleSelect4">Price</label>
+        <br />
+        <input type="number" value="0" min="0" step="0.01" data-number-to-fixed="2" data-number-stepfactor="100" class="form-control" name="price" id="c1" value="{{ old('price')}}" />
+        <br />
+        <br />
+        <button type="submit" class="btn btn-primary" value="Upload">Upload</button>
+        <br />
+        <br />
+      </form>
+    </div>
   </div>
-</div>
 
 </div>
 

05dfd3b9db55c647a9c57a007c3c501f029e3c0f
CharlieVS92 <31687886+CharlieVS92@users.noreply.github.com>
2018-12-09 20:45:06 +0100

    Add to Cart & Wishlist
    Not redirected to page, but a flash message is shown after adding a product to your cart/wishlist

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,6 +36,7 @@
 </head>
 <body>
   <div id="app">
+    @include('flash-message')
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <a class="navbar-brand" href="{{ url('/')}}">MUZEN</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">

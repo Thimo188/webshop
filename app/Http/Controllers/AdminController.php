6888a383a57cdb79d3af6d4b71c794659bf124ab
Omid <31657634+OmidSahebzadeh@users.noreply.github.com>
2018-12-14 12:51:47 +0100

    AdminController updates :)

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -15,8 +15,229 @@ Use Auth;
 class AdminController extends Controller
 {
     public function index() {
+      $productsArray = Product::orderBy('product_name')
+      ->pluck('product_name')
+      ->toArray();
 
-      return view('/admin');
+
+      // $chart = Charts::create('bar', 'highcharts')
+      //     ->title("Product Details")
+      //     ->elementLabel("Total Products")
+      //     ->dimensions(1000, 500)
+      //     ->responsive(true);
+      //     //->groupByMonth(date('Y'), true);
+
+  $SubSold = DB::table('subscriptions')
+    ->select(DB::raw('SUM(amount) as amount'), DB::raw("CONCAT_WS('-',MONTH(created_at),YEAR(created_at)) as monthyear"))
+    ->groupBy(DB::raw('YEAR(created_at) ASC, MONTH(created_at) ASC, monthyear'))
+    ->where( 'created_at', '>=', DB::raw( 'LAST_DAY(CURRENT_DATE) + INTERVAL 1 DAY - INTERVAL 1 YEAR',
+      'AND', 'created_at',  '<', ' LAST_DAY(CURRENT_DATE) + INTERVAL 1 DAY'))
+    ->pluck('amount')
+   ->toArray();
+
+  $MonthAndYear = DB::table('order_details')
+    ->select(DB::raw('SUM(amount) as total_expense'), DB::raw("CONCAT_WS('-',MONTH(created_at),YEAR(created_at)) as monthyear"))
+    ->groupBy(DB::raw('YEAR(created_at) ASC, MONTH(created_at) ASC, monthyear'))
+    ->where( 'created_at', '>=', DB::raw( 'LAST_DAY(CURRENT_DATE) + INTERVAL 1 DAY - INTERVAL 1 YEAR',
+      'AND', 'created_at',  '<', ' LAST_DAY(CURRENT_DATE) + INTERVAL 1 DAY'))
+    ->pluck('monthyear')
+   ->toArray();
+
+   $SubMonthYear = DB::table('subscriptions')
+     ->select(DB::raw('SUM(amount) as total_expense'), DB::raw("CONCAT_WS('-',MONTH(created_at),YEAR(created_at)) as monthyear"))
+     ->groupBy(DB::raw('YEAR(created_at) ASC, MONTH(created_at) ASC, monthyear'))
+     ->where( 'created_at', '>=', DB::raw( 'LAST_DAY(CURRENT_DATE) + INTERVAL 1 DAY - INTERVAL 1 YEAR',
+       'AND', 'created_at',  '<', ' LAST_DAY(CURRENT_DATE) + INTERVAL 1 DAY'))
+     ->pluck('monthyear')
+    ->toArray();
+
+
+
+    $Soldquantity = DB::table('order_details')
+    ->select(DB::raw('SUM(amount) as total_expense'), DB::raw("CONCAT_WS('-',MONTH(created_at),YEAR(created_at)) as monthyear"))
+    ->groupBy(DB::raw('YEAR(created_at) ASC, MONTH(created_at) ASC, monthyear'))
+    ->where( 'created_at', '>=', DB::raw( 'LAST_DAY(CURRENT_DATE) + INTERVAL 1 DAY - INTERVAL 1 YEAR',
+      'AND', 'created_at',  '<', ' LAST_DAY(CURRENT_DATE) + INTERVAL 1 DAY'))
+    ->pluck('total_expense')
+    ->toArray();
+
+
+
+  $Soldquantity1 = DB::table('order_details')
+  ->join('products', 'order_details.product_id', '=', 'products.id')
+  ->select('order_details.product_id', DB::raw('SUM(order_details.amount)as total'))
+  ->groupBy('order_details.product_id')
+  ->orderBy('order_details.created_at', 'asc')
+  ->pluck('total')->toArray();
+
+  $Soldproducts = DB::table('order_details')
+  ->join('products', 'order_details.product_id', '=', 'products.id')
+  ->select('order_details.product_id', DB::raw('SUM(order_details.amount)as total'))
+  ->groupBy('order_details.product_id')
+  ->orderBy('total','asc')
+  ->take(5)
+  ->pluck('total')->toArray();
+
+  $SoldName = DB::table('order_details')
+  ->join('products', 'order_details.product_id', '=' , 'products.id')
+  ->select('order_details.product_id','products.product_name', DB::raw('SUM(order_details.amount)as total'))
+  ->groupBy('order_details.product_id', 'products.product_name')
+  ->orderBy('total', 'asc')
+  ->pluck('products.product_name')->toArray();
+
+
+  $Month = DB::table('order_details')
+    ->select('order_details.product_id', 'order_details.created_at', DB::raw('SUM(order_details.amount)as total'))
+    ->groupBy('order_details.product_id', 'order_details.created_at')
+    ->orderBy('order_details.created_at', 'asc')
+    ->pluck('order_details.created_at')->toArray();
+
+
+    $Price = DB::table('order_details')
+    ->select(DB::raw('SUM(amount * product_price) as total'), DB::raw("CONCAT_WS('-',MONTH(created_at),YEAR(created_at)) as monthyear"))
+    ->groupBy(DB::raw('YEAR(created_at) ASC, MONTH(created_at) ASC, monthyear'))
+    ->where( 'created_at', '>=', DB::raw( 'LAST_DAY(CURRENT_DATE) + INTERVAL 1 DAY - INTERVAL 1 YEAR',
+      'AND', 'created_at',  '<', ' LAST_DAY(CURRENT_DATE) + INTERVAL 1 DAY'))
+  //  ->orderByRaw('')
+    ->pluck('total')
+   ->toArray();
+  // $Price = DB::table('order_details')
+  // ->select('order_details.product_id', 'order_details.product_price', 'order_details.amount', DB::raw('(order_details.product_price * order_details.amount)as price'))
+  // ->groupBy('order_details.product_id', 'order_details.product_price', 'order_details.amount')
+  // ->orderBy('price', 'asc')
+  // ->pluck('order_details.product_price')->toArray();
+
+  $PerDay = DB::table('order_details')
+      ->select(DB::raw('SUM(order_details.amount) as total'), DB::raw("CONCAT_WS('-',DAY(created_at),MONTH(created_at),YEAR(created_at)) as daymonthyear"))
+      ->groupBy(DB::raw('YEAR(created_at) ASC, MONTH(created_at) ASC,DAY(created_at) ASC, daymonthyear'))
+      ->where( 'created_at', '>=', DB::raw( 'LAST_DAY(CURRENT_DATE) + INTERVAL 1 DAY - INTERVAL 1 MONTH',
+        'AND', 'created_at',  '<', ' LAST_DAY(CURRENT_DATE) + INTERVAL 1 DAY'))
+      ->pluck('daymonthyear')
+      ->toArray();
+
+  $PerDayValue = DB::table('order_details')
+     ->select(DB::raw('SUM(amount) as total_expense, MONTH(created_at) as month, YEAR(created_at) as year,DAY(created_at) as day'))
+     ->groupBy(DB::raw('YEAR(created_at) ASC, MONTH(created_at) ASC,DAY(created_at) ASC'))
+     ->where( 'created_at', '>=', DB::raw( 'LAST_DAY(CURRENT_DATE) + INTERVAL 1 DAY - INTERVAL 1 MONTH',
+       'AND', 'created_at',  '<', ' LAST_DAY(CURRENT_DATE) + INTERVAL 1 DAY'))
+     ->pluck('total_expense', 'day')
+     ->toArray();
+
+  $chart_subs = Charts::database(Subscription::all(),'bar', 'highcharts')
+    ->title('Subscriptions bought')
+    ->elementLabel('Subscriptions')
+    ->labels($SubMonthYear)
+    ->values($SubSold)
+    ->dimensions(1500,500)
+    ->responsive(true)
+    ->groupByMonth('2018', true);
+
+// hoeveel opbrengst
+  $line_chart = Charts::create('line', 'highcharts')
+    ->title('Turnover')
+    ->elementLabel('turnover')
+    ->labels($MonthAndYear)
+    ->values($Price)
+    ->dimensions(1500,500)
+    ->responsive(true);
+
+
+// chart voor hoeveel producten per maand
+  $chart = Charts::database(Order_Detail::all(),'bar', 'highcharts')
+    ->title('Products sold')
+    ->elementLabel('Products')
+    ->labels($MonthAndYear)
+    ->values($Soldquantity)
+    ->dimensions(1500,500)
+    ->responsive(true);
+    // ->groupByDay('30','2016',true);
+
+// chart voor welke producten het meest verkocht zijn
+  $pie_chart = Charts::create('pie', 'highcharts')
+      ->title('Popular product sales')
+      ->elementLabel('Products')
+      ->labels($SoldName)
+      ->values($Soldproducts)
+      ->dimensions(1500,500)
+      ->responsive(true);
+
+  // $chart = Charts::create('bar', 'highcharts')->dateColumn('created_at')
+  //     ->title('Products sold')
+  //     ->elementLabel('Products')
+  //     ->labels($SoldName)
+  //     ->values($Soldproducts)
+  //     ->dimensions(1500,500)
+  //     ->responsive(true)
+  //     ->groupByMonth();
+
+
+  // $chart = Charts::database(User::all(), 'bar', 'highcharts')->data(Role::all())
+  //   ->title('names')
+  //   ->elementLabel('Total')
+  //   ->dimensions(1000,500)
+  //   ->responsive(false)
+  //   ->groupBy('name');
+
+  // $pie_chart = Charts::create('pie', 'highcharts')
+  //     ->title('Products sold')
+  //     ->labels($SoldName)
+  //     ->values($Soldproducts)
+  //     ->dimensions(1500,500)
+  //     ->responsive(true);
+
+
+
+
+  // $line_chart = Charts::create('line', 'highcharts')
+  //       ->title('Line Chart Demo')
+  //       ->elementLabel('Chart Labels')
+  //       ->labels(['Product 1', 'Product 2', 'Product 3', 'Product 4', 'Product 5', 'Product 6'])
+  //       ->values([15,25,50, 5,10,20])
+  //       ->dimensions(1000,500)
+  //       ->responsive(true);
+
+  $areaspline_chart = Charts::multi('areaspline', 'highcharts')
+          ->title('Areaspline Chart Demo')
+          ->colors(['#ff0000', '#ffffff'])
+          ->labels(['Jan', 'Feb', 'Mar', 'Apl', 'May','Jun'])
+          ->dataset('Product 1', [10, 15, 20, 25, 30, 35])
+          ->dataset('Product 2',  [14, 19, 26, 32, 40, 50]);
+
+
+  $percentage_chart = Charts::create('percentage', 'justgage')
+          ->title('Percentage Chart Demo')
+          ->elementLabel('Chart Labels')
+          ->values([65,0,100])
+          ->responsive(true)
+          ->height(300)
+          ->width(0);
+
+  $geo_chart = Charts::create('geo', 'highcharts')
+          ->title('GEO Chart Demo')
+          ->elementLabel('Chart Labels')
+          ->labels(['US', 'UK', 'IND'])
+          ->colors(['#C5CAE9', '#283593'])
+          ->values([25,55,70,90])
+          ->dimensions(1000,500)
+          ->responsive(true);
+
+  $area_chart = Charts::create('area', 'highcharts')
+        ->title('Area Chart')
+        ->elementLabel('Chart Labels')
+        ->labels(['First', 'Second', 'Third'])
+        ->values([28,52,64,86,99])
+        ->dimensions(1000,500)
+        ->responsive(true);
+
+  $donut_chart = Charts::create('donut', 'highcharts')
+        ->title('Donut Chart')
+        ->labels(['Product 1', 'Product 2', 'Product 3', 'Product 4', 'Product 5', 'Product 6'])
+        ->values([25,50,70,860])
+        ->dimensions(1000,500)
+        ->responsive(true);
+
+      return view('/adminproducts',compact('chart','chart_subs' , 'pie_chart', 'line_chart', 'areaspline_chart', 'percentage_chart', 'geo_chart', 'area_chart', 'donut_chart'));
+
 
 
 
@@ -159,7 +380,7 @@ class AdminController extends Controller
     ->values($Soldquantity)
     ->dimensions(1500,500)
     ->responsive(true);
-    // ->groupByMonth('2018',true);
+    // ->groupByDay('30','2016',true);
 
 // chart voor welke producten het meest verkocht zijn
   $pie_chart = Charts::create('pie', 'highcharts')

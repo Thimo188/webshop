4b809564aebce3b4bf1a5ced03493f9338ee1b49
Vincent van Tilborg <43572377+VincentvanTil@users.noreply.github.com>
2019-01-06 22:08:55 +0100

    3D pagina fixed (airplane removed)

--- resources/views/3DArt.blade.php
+++ resources/views/3DArt.blade.php
@@ -40,37 +40,27 @@
 					</div>
 				</div>
 			</div>
-
 			<div class="col-md-9">
 				<div class="row">
 					@forelse($productsview as $product)
 					<div class="col-md-4 card-margin d-flex align-items-stretch">
 						<div class="card">
-							<a href="/description/{{$product->id}}">
-							<img class="card-img-top" src="{{asset($product->file)}}" alt="Card image cap"></a>
+						<a href="/description/{{$product->id}}"><img class="card-img-top" src="{{asset($product->ProductImages->file)}}" alt="Card image cap"></a>
 							<div class="card-body">
 								<h5 class="card-title">{{$product->product_name}}</h5>
             					<p class="card-text">{{ str_limit($product->product_description, 80) }}</p>
 							</div>
 							<div class="card-footer">
-								<div class="center" style="margin:50px;">
-								  <button class="planebtn">
-								    <span>SEND</span>
-								    <i class="fa fa-paper-plane fa-lg replace"></i>
-								    <i class="fa fa-paper-plane plane fa-lg hidden"></i>
-								  </button>
-								</div>
-								<a href="{{ url('/addToCart', $product->id) }}" class="btn btn-lg btn-light"><i class="fas fa-shopping-cart top" id="carticon"></i></a>
+							  <a href="{{ url('/addToCart', $product->id) }}" class="btn btn-lg btn-light"><i class="fas fa-shopping-cart top" id="carticon"></i></a>
 								<p style="float: right">€ {{ number_format($product->price, 2,'.',',')}}</p>
 								<a href="{{ Route('wishlist.add', $product->id) }}" class="btn btn-lg btn-light">
 									<img src="https://cdn3.iconfinder.com/data/icons/pyconic-icons-1-2/512/heart-outline-512.png" height="25" class="hello" alt=""/></a>
 							</div>
-							@empty
-							<p> no posts found </p>
-							@endforelse
 						</div>
 					</div>
-
+					@empty
+					<p> no posts found </p>
+					@endforelse
 				</div>
 			</div>
 		</div>
